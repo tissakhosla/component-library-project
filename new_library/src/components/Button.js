9d90a714ccde82538c242cf8a6774c98b5ff8b4f
Tissa Khosla <tissa.music@gmail.com>
2019-10-31 10:35:37 -0400

    working on buttones

--- new_library/src/components/Button.js
+++ new_library/src/components/Button.js
@@ -3,9 +3,18 @@ import './Button.css'
 // This is a functional component - just sent up a little differently as an arrow function!
 const Button = (props) => {
   let classList = ''
-  let types = ['primary', 'danger', 'success', 'warning']
+  let types = ['primary', 'danger', 'success', 'warning', 'default']
   if (types.includes(props.type)){
     classList += ` button-${props.type}`
+  }
+  if (props.white) {
+    classList += ` button-${props.type}-white`
+  }
+  if (props.inverse){
+    classList += ` button-${props.type}-inverse`
+  }
+  if (props.hover){
+    classList += ` button-hover`
   }
     return (
       <button className={classList}>
--- new_library/src/components/Button.stories.js
+++ new_library/src/components/Button.stories.js
@@ -3,6 +3,19 @@ import { storiesOf } from '@storybook/react';
 import Button from './Button';
 
 storiesOf('Buttons', module)
-    .add('Primary', () => <Button label="Primary" type="primary"/>)
-
+    .add('Primary', () => <Button label="Do something" type="primary" hover/>)
+    .add('Primary white', () => <Button label="Do something" type="primary" white/>)
+    .add('Primary inverse', () => <Button label="Do something" type="primary" inverse/>)
+    .add('Danger', () => <Button label="Do something" type="danger" hover/>)
+    .add('Danger white', () => <Button label="Do something" type="danger" white/>)
+    .add('Danger inverse', () => <Button label="Do something" type="danger" inverse/>)
+    .add('Success', () => <Button label="Do something" type="success" hover/>)
+    .add('Success white', () => <Button label="Do something" type="success" white/>)
+    .add('Success inverse', () => <Button label="Do something" type="success" inverse/>)
+    .add('Warning', () => <Button label="Do something" type="warning" hover/>)
+    .add('Warning white', () => <Button label="Do something" type="warning" white/>)
+    .add('Warning inverse', () => <Button label="Do something" type="warning" inverse/>)
+    .add('Default', () => <Button label="Do something" type="default" hover/>)
+    .add('Default white', () => <Button label="Do something" type="default" white/>)
+    .add('Default inverse', () => <Button label="Do something" type="default" inverse/>)
     
