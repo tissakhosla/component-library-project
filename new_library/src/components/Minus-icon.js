3c8b47947b9d3f549147f7ab71e881298de27a5d
Tissa Khosla <tissa.music@gmail.com>
2019-10-31 16:26:46 -0400

    icon components created

---
+++ new_library/src/components/Minus-icon.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import minus from '../icons/minus.svg'
+
+
+const Minus = () => {
+  console.log("Something")
+  return (
+    <div>
+      <img src={minus}></img>
+    </div>
+  )
+}
+export default Minus;
---
+++ new_library/src/components/Plus-icon.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import plus from '../icons/plus.svg'
+
+
+const Plus = () => {
+  console.log("Something")
+  return (
+    <div>
+      <img src={plus}></img>
+    </div>
+  )
+}
+export default Plus;
