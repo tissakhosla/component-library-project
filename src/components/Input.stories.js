fe538d8b571b396af9c4dca53d1ea1a1f3e3cc25
Tissa Khosla <tissa.music@gmail.com>
2019-11-01 11:21:44 -0400

    created inputWithSubmit

--- src/components/Input.stories.js
+++ src/components/Input.stories.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
-
 import Input from './Input';
+import InputWithSubmit from './InputWithSubmit'
 
 storiesOf('Text Fields', module)
   .add("Input default", () => <Input size="default" value="Email"></Input>)
   .add("Input ml", () => <Input size='ml' value="Email"></Input>)
   .add("Input l", () => <Input size='l' value="Email"></Input>)
+  .add("Input with Button", () => <InputWithSubmit value="GET IT" placeholder="whatever you want"></InputWithSubmit>)
---
+++ src/components/InputWithSubmit.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react'
+import './InputWithSubmit.css'
+
+class InputWithSubmit extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      value: this.props.value,
+      placeholder: this.props.placeholder
+    }
+    this.handleSubmit = this.handleSubmit.bind(this)
+  }
+
+  handleSubmit(eo) {
+    eo.preventDefault()
+    eo.persist()
+    alert(`You want ${eo.target[0].value}?!`)
+  }
+
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit} className="input-with-submit">
+        <input className="text-field" type="text" placeholder={this.state.placeholder}></input>
+        <input className="submit" value={this.state.value} type="submit"></input>
+      </form>
+    )
+  }
+}
+
+export default InputWithSubmit;
