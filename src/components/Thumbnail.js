01e0e9d5a1250a65d226a9da7fabd5a177c34f0a
Tissa Khosla <tissa.music@gmail.com>
2019-11-25 21:48:45 -0500

    basic thumbnail completed  - need to do headphones thumbnail ans  - inline thumbnail

--- src/components/Thumbnail.js
+++ src/components/Thumbnail.js
@@ -1,25 +1,44 @@
 import React from 'react';
 import './Thumbnail.css';
-import pic from '../icons/macbook.jpg'
 import yellowStar from '../icons/starcopy.svg'
 import greyStar from '../icons/starcopy2.svg'
 
 const Thumbnail = (props) => {
+
+  let thumbClass = `thumbnail-${props.shade}-${props.orientation}`
+  let picClass = `pic-${props.shade}`
+  let ratingsClass = `ratings-${props.orientation}`
+  let infoClass = `info-${props.orientation}`
+  let lineClass = `line-${props.orientation}`
+  let cartColorClass, heartColorClass
+  
+  if (props.shade === 'dark') {
+    cartColorClass = "blue-cart"
+    heartColorClass = "blue-heart"
+  } else {
+    cartColorClass = "grey-cart"
+    heartColorClass = "grey-heart"
+  }
+
+
+
   return (
-    <div className="thumbnail">
-      <div className="pic">
-      <div className="buttons"><button className="grey-cart"></button><button className="grey-heart"></button></div>
+    <div className={thumbClass}>
+      <div className={picClass}>
+        <div className="buttons"><button className={cartColorClass}></button><button className={heartColorClass}></button></div>
       </div>
-      <div className="line"></div>
-      <p className="title">{props.product}</p>
-      <div className="ratings">
-        <img src={yellowStar} />
-        <img src={yellowStar} />
-        <img src={yellowStar} />
-        <img src={yellowStar} />
-        <img src={greyStar} />
+      <div className={lineClass}></div>
+      <div className={infoClass}>
+        <p className="title">{props.product}</p>
+        <div className={ratingsClass}>
+          <img src={yellowStar} />
+          <img src={yellowStar} />
+          <img src={yellowStar} />
+          <img src={yellowStar} />
+          <img src={greyStar} />
+        </div>
+        <p className="new-cost">${props.discount}    <span className="old-cost">  ${props.retail}</span></p>
       </div>
-      <p className="new-cost">${props.discount}    <span className="old-cost">  ${props.retail}</span></p>
     </div>
   )
 }
--- src/components/Thumbnail.stories.js
+++ src/components/Thumbnail.stories.js
@@ -3,13 +3,33 @@ import { storiesOf } from '@storybook/react';
 import Thumbnail from './Thumbnail'
 
 storiesOf('Thumbnails', module)
-  .add('Dark Thumbnail', () => <Thumbnail
+  .add('Dark Portrait', () => <Thumbnail
+    orientation='portrait'
+    shade='dark'
     pic="../icons/macbook.jpg"
     product="Apple Macbook Pro"
     retail={599}
     discount={499}
     outline />)
-  .add('Light Thumbnail', () => <Thumbnail
+  .add('Light Portrait', () => <Thumbnail
+    orientation='portrait'
+    shade='light'
+    pic="../icons/macbook.jpg"
+    product="Apple Macbook Pro"
+    retail={599}
+    discount={499}
+    outline />)
+  .add('Dark Landscape', () => <Thumbnail
+    orientation='landscape'
+    shade='dark'
+    pic="../icons/macbook.jpg"
+    product="Apple Macbook Pro"
+    retail={599}
+    discount={499}
+    outline />)
+  .add('Light Landscape', () => <Thumbnail
+    orientation='landscape'
+    shade='light'
     pic="../icons/macbook.jpg"
     product="Apple Macbook Pro"
     retail={599}
